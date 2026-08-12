0fb471e9fd69dbf20ac8694b13137f4e54097d14
AhmedSayed2200 <ahmedsayed2200737@gmail.com>
2026-08-13 00:58:06 +0300

    Update new features

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,22 +2,6 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'category/:slug/:id',
-    renderMode: RenderMode.Client
-  },
-  {
-    path: 'brand/:slug/:id',
-    renderMode: RenderMode.Client
-  },
-  {
-    path: 'details/:slug/:id',
-    renderMode: RenderMode.Client
-  },
-  {
-    path: 'checkout/:id',
-    renderMode: RenderMode.Client
-  },
-  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
