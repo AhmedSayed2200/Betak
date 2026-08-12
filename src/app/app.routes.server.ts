fb9e0b6267f60e392c3f3be37c892e7f32ebba82
AhmedSayed2200 <ahmedsayed2200737@gmail.com>
2026-08-12 22:38:14 +0300

    Fix prerender routing for vercel

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,22 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'category/:slug/:id',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'brand/:slug/:id',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'details/:slug/:id',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'checkout/:id',
+    renderMode: RenderMode.Client
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
