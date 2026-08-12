f346870ab961aeea65fa19d4f88caffa873cb76f
AhmedSayed2200 <ahmedsayed2200737@gmail.com>
2026-08-13 01:15:11 +0300

    Fix SSR server routes for Render build

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,23 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'category/:slug/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'brand/:slug/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'details/:slug/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'checkout/:id',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Server
   }
 ];
